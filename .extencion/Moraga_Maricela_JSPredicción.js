278495d4e00a89787cf45c700c4858240c6749af
maricela <moraga.orellana@gmail.com>
2022-09-02 12:13:58 -0400

    pre coding

---
+++ .extencion/Moraga_Maricela_JSPredicción.js
@@ -0,0 +1,9 @@
+// 1. Naci en 1980
+
+// 2. Naci en 1980 
+
+// 3. ¡Sumando números!
+//    num1 is: 10
+//    num2 is: 20 
+//    30 
+
---
+++ .extencion/Moraga_Maricela_JS_Jugueteo.js
@@ -0,0 +1,10 @@
+function muestraSiElNiñoPuedeSubirALaMontañaRusa(){
+    if (alturaNiño > 52){
+        console.log("¡Súbete, chico!");
+    }
+    else{
+        console.log ("Lo siento, chico. Tal vez el próximo año");
+    }
+    
+var alturaNiño = 50;
+}
